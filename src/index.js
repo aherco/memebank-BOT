1133528526d2a7c47df2da286e88ebe1837bdfbb
aherco <herco.anthony@gmail.com>
2019-04-08 22:51:53 -0700

    add and delete message collectors from bmc on guild/channel create/delete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import Discord from 'discord.js';
 import BatchMessageCollector from './BatchMessageCollector.js';
 
 const bot = new Discord.Client();
-const bmcs = new BatchMessageCollector(bot, (channel, collection) => { return channel.type === 'text'; });
+const bmc = new BatchMessageCollector(bot, (channel, collection) => { return channel.type === 'text'; });
 
-bmcs.on('addChannel', bmcs.listener);
-bot.on('ready', () => { bot.channels.tap((channel) => { bmcs.emit('addChannel', channel); }); });
+// adds channel to bmc
+const addChannel = (channel) => { bmc.emit('addChannel', channel); };
+const deleteChannel = (channel) => { bmc.collected.delete(channel.id); };
+bmcs.on('addChannel', bmc.listener);
+
+bot.on('ready', () => { bot.channels.tap(addChannel); });
 bot.on('error', console.error);
+
+bot.on('guildCreate', (guild) => { guild.channels.tap(addChannel); });
+bot.on('guildDelete', (guild) => { guild.channels.tap(deleteChannel); });
+
+bot.on('channelCreate', addChannel);
+bot.on('channelDelete', deleteChannel);
+
 
 bot.login(process.env.TOKEN);
