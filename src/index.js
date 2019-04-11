a84bfc6e0233e5a11aa7405f3b739affc2205fb7
aherco <herco.anthony@gmail.com>
2019-04-11 16:32:39 -0700

    respond with memebank link when bot is mentioned

--- src/index.js
+++ src/index.js
@@ -23,5 +23,21 @@ bot.on('guildDelete', (guild) => { guild.channels.tap(deleteChannel); });
 bot.on('channelCreate', addChannel);
 bot.on('channelDelete', deleteChannel);
 
+bot.on('message', (message) => {
+  if (message.mentions.users.has(bot.user.id) && bmc.collected.has(message.channel.id)) {
+
+    const response = new Discord.RichEmbed()
+      .setTitle(`View #${message.channel.name}'s memebank here!`)
+      .setDescription(`This message will self destruct in 10 seconds.`)
+      .setTimestamp()
+      .setThumbnail('https://cdn.discordapp.com/attachments/561521713901338635/566031952684253194/unknown.png')
+      .setURL(`http://localhost:3000/${message.channel.guild.id}`)
+      .setColor(0x1f912f)
+
+    message.delete();
+    message.channel.send(response).then((msg) => { msg.delete(10000); });
+  }
+});
+
 
 bot.login(process.env.TOKEN);
