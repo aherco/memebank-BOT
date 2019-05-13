fcf14c020a06ed75e5a2d85495f89db33f8f9a34
aherco <herco.anthony@gmail.com>
2019-05-13 07:22:16 -0700

    send vote link with memebank link

--- src/index.js
+++ src/index.js
@@ -39,16 +39,30 @@ bot.on('message', (message) => {
   if (message.mentions.users.has(bot.user.id) && bmc.collected.has(message.channel.id) && !message.author.bot) {
 
     const url = `${process.env.DOMAIN}/${message.channel.guild.name.replace(/ /g, '-')}/${message.channel.name}/${message.channel.id}`;
+
     const response = new Discord.RichEmbed()
       .setTitle(`View #${message.channel.name}'s memebank here!`)
-      .setDescription(`This message will self destruct in 10 seconds.`)
-      .setTimestamp()
-      .setThumbnail('https://cdn.discordapp.com/attachments/561521713901338635/566031952684253194/unknown.png')
+      .setDescription('This message will self destruct in 10 seconds.')
+      .setThumbnail(process.env.THUMBNAIL)
       .setURL(url)
       .setColor(0x1f912f)
+    ;
+
+    const vote = new Discord.RichEmbed()
+      .setTitle('Vote for memebank!')
+      .setDescription('Please support us by voting on discortbots.org!')
+      .setThumbnail(process.env.THUMBNAIL)
+      .setURL(process.env.VOTE)
+      .setColor(0x1f912f)
+    ;
 
     message.delete().catch(console.error);
-    message.channel.send(response).then((msg) => { msg.delete(10000); }).catch(console.error);
+    message.channel.send(response).then((msg) => { 
+      msg.delete(10000);
+      message.channel.send(vote).then((msg) => {
+        msg.delete(10000);
+      }).catch(console.error);
+    }).catch(console.error);
   }
 });
 
